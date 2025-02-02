6dba18f327411c158fcbaf6d17e6f4262f011dc1
Mathieu <mathieuppicard@hotmail.com>
2025-02-02 12:09:36 -0500

    Add classifier 5 classes to MLP

--- MLP.py
+++ MLP.py
@@ -4,6 +4,12 @@ import MLP_Cla as Classifier
 import numpy as np
 import sys
 
+###TODO###
+#Loop while augmenting the number of 1-images to train, 2- the number of hidden layer and 3- the number of neuronnes
+    # The results of the models need to be save
+    # With the result plot and analyse the best combinaison of factors.
+    # Make a interractive dashboard???
+
 def main():
     Generator.main(generateImage=True,ok=4,nok=12,color=None)
 
@@ -11,17 +17,13 @@ def main():
     #print(array_2x)
     #print(array_2y)
 
-    Classifier.main(array_2x,array_2y)
+    Classifier.train(array_2x,array_2y)
 
-    #array_5x, array_5y = Analyser.main(5)
+    array_5x, array_5y = Analyser.main(5)
     #print(array_5x)
     #print(array_5y)
 
-    # Loop while augmenting the number of images to train, the number of hidden layer and the number of neuronnes
-    # The results of the models need to be save
-    # With the result plot and analyse the best combinaison of factors.
-    # Make a interractive dashboard???
-
+    Classifier.train(array_5x,array_5y)
 
 if __name__ ==  "__main__" :
     sys.exit(main())
