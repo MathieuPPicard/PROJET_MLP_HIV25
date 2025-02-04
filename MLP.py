de5623514c97b1872aa047c7602c2cb4c0201153
Mathieu <mathieuppicard@hotmail.com>
2025-02-04 18:40:24 -0500

    Rework base model, refactor Iterator function. Now using a set of tuple for layers. Add deleting image folder

--- MLP.py
+++ MLP.py
@@ -4,52 +4,56 @@ import DataCollector as Collector
 import MLP_Cla as Classifier
 import numpy as np
 import sys
+import shutil
 
 N_INPUT = 17 * 17
 
 ###TODO###
-# DELETE THE OK AND NOK FOLDER EACH TIME AT THE END OF CapGenerator main
+# DELETE THE OK AND NOK FOLDER [X]
 # Loop while increasing the number of 1-Data sizes, 2-number of hidden layer and 3-the number of neuronnes [X]
+# Use data augmentation to get less overfitting
+# Create a evaluation function(weigh the following : average, standard deviation, time of computation, number of image)
 # The results of the models need to be save with DataCollector.py
 # With the results, plot and analyse the best combinaison of factors.
 # Make a interractive dashboard???
 
-def iterate(num_max_layer, num_max_image, classes, num_model_training=1) :
-    layer_sizing = []
-    size = N_INPUT
+def iterate(classes, num_max_image=100, num_model_training=1) :
+    layer_sizing = [(64, 32), (128, 64), (128, 64, 32)]
     #Increasing data size
-    for i in range(10,num_max_image+1,10) :
-        print(f"Images genere : {i*2}")
+    for i in range(10,num_max_image + 1,10) :
+        print(f"Images genere par classes : {i}")
         #Incresing hidden layer number while decresing the number of neurons
-        for y in range(1,num_max_layer+1,1) :
-            layer_sizing.append(int(np.floor(size/y)))
-            result = getAverage(num_model_training,i,i,tuple(layer_sizing),classes)
-            print(f"Average for {y} layers: {result}")    
+        for y in range(3) :
+            result = trainModel(n=num_model_training,n_ok=i,n_nok=i*4,layer_size=layer_sizing[y],classes=classes)
+            print(f"Average for {i} image, {str(layer_sizing[y])}: {result}")
             #
             # Add the collection of data here.
-            # Will need average, Standard deviation, time of computation, number of images used, number of hidden layers, number of neurons(total) and how may classes  
+            # Will need eval fonction ,average , Standard deviation, time of computation, number of images used, number of hidden layers, number of neurons(total) and how may classes  
             # Collector.save()
             #
-            
-        layer_sizing = []
 
 #Get the average of n time of the same model.
-def getAverage(n, n_ok=25, n_nok=100, layer_size=(220,), classes=2) :
+def trainModel(n, n_ok=25, n_nok=100, layer_size=(220,), classes=2) :
     Generator.main(generateImage=True,ok=n_ok,nok=n_nok,color=None)
     array_2x, array_2y = Analyser.main(classes)
     total = 0
     for i in range(n) :
         result = Classifier.train(array_2x,array_2y, layer_size)
         total += result
+    deleteImageFolder()
     return total / n
 
-def main():
-    iterate(num_max_layer=6, num_max_image=167, classes=2, num_model_training=1)
+def deleteImageFolder() :
+    try:
+        folder_path1 = 'OK'
+        folder_path2 = 'NOK'
+        shutil.rmtree(folder_path1)
+        shutil.rmtree(folder_path2)
+    except:
+        print('One of the image folder could not be deleted')
 
-    #array_5x, array_5y = Analyser.main(5)
-    #print(array_5x)
-    #print(array_5y)
-    #Classifier.train(array_5x,array_5y) 
+def main():
+    iterate(num_max_image=200, classes=2, num_model_training=1)
 
 if __name__ ==  "__main__" :
     sys.exit(main())
--- MLP_Cla.py
+++ MLP_Cla.py
@@ -12,6 +12,8 @@ def transformArray(array) :
     return array
 
 def train(x_array, y_array, layer_size) :
+
+    
     # Step 1: Preprocess the data (flattening)
     x_array = transformArray(np.array(x_array))
     y_array = np.array(y_array)
@@ -19,18 +21,35 @@ def train(x_array, y_array, layer_size) :
     # Step 2: Split into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(x_array, y_array, test_size=0.2, random_state=42)
 
-    # Step 3: Create MLPClassifier model
-    mlp = MLPClassifier(hidden_layer_sizes=layer_size, max_iter=1000, activation='relu', solver='lbfgs')
+    # Step 3: Scale
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
-    # Step 4: Train the model
+    # Step 4: Create MLPClassifier model
+    mlp = MLPClassifier(hidden_layer_sizes=layer_size,
+        max_iter=1000, 
+        activation='relu',
+        solver='adam',
+        alpha=0.01,
+        early_stopping=True,
+        n_iter_no_change=10)
+
+    # Step 5: Train the model
     mlp.fit(X_train, y_train)
 
-    # Step 5: Predict with the trained model
+    # Step 6: Predict with the trained model
     y_pred = mlp.predict(X_test)
     
-    # Step 6: Evaluate accuracy
+    # Step 7: Evaluate
     accuracy = accuracy_score(y_test, y_pred)
     #print(f"Accuracy: {accuracy * 100:.2f}%")
+
+    train_accuracy = mlp.score(X_train, y_train)
+    test_accuracy = mlp.score(X_test, y_test)
+    print(f"Training Accuracy: {train_accuracy * 100:.2f}%")
+    print(f"Testing Accuracy: {test_accuracy * 100:.2f}%")
+
     return accuracy
 
 def main(x_array, y_array ) :
