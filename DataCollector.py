9183f4c64970de5bc39874ba53ba0fc543315edd
Mathieu <mathieuppicard@hotmail.com>
2025-02-04 20:49:59 -0500

    Made dataCollector functions, add the colloction in the iteration function.

--- DataCollector.py
+++ DataCollector.py
@@ -1,2 +1,27 @@
-def save():
-    pass
+import pandas as pd
+import os
+
+CSV_PATH = 'data.csv'
+
+def save(evaluation, average, nRange, n_image, n_layers, n_neurons,computation_time) :
+    data = {
+        'number_image' : n_image,
+        'number_layer' : n_layers,
+        'number_neurons' : n_neurons,
+        'evaluation' : evaluation,
+        'average' : average,
+        'range' : nRange,
+        'computation_time' : computation_time  
+    }
+    df = pd.DataFrame([data])
+
+    if not os.path.exists(CSV_PATH) :
+        df.to_csv(CSV_PATH, mode='w', index=False, header=True)
+    else :
+        df.to_csv(CSV_PATH, mode="a", index=False, header=False)
+
+def deleteData() :
+    try:
+        os.remove(CSV_PATH)
+    except:
+        print('The csv file could not be remove.')
--- MLP.py
+++ MLP.py
@@ -5,6 +5,7 @@ import MLP_Cla as Classifier
 import numpy as np
 import sys
 import shutil
+import time
 
 N_INPUT = 17 * 17
 
@@ -12,36 +13,58 @@ N_INPUT = 17 * 17
 # DELETE THE OK AND NOK FOLDER [X]
 # Loop while increasing the number of 1-Data sizes, 2-number of hidden layer and 3-the number of neuronnes [X]
 # Use data augmentation to get less overfitting
-# Create a evaluation function(weigh the following : average, standard deviation, time of computation, number of image)
-# The results of the models need to be save with DataCollector.py
+# Create a evaluation function(weigh the following : average, range, time of computation, number of image + number of layer(neurons))
+# The results of the models need to be save with DataCollector.py [ X ]
 # With the results, plot and analyse the best combinaison of factors.
 # Make a interractive dashboard???
 
 def iterate(classes, num_max_image=100, num_model_training=1) :
-    layer_sizing = [(64, 32), (128, 64), (128, 64, 32)]
+    layer_sizing = [(64,), (128,), (64, 32), (128, 64), (128, 64, 32)]
+    layer_size = [1,1,2,2,3]
     #Increasing data size
     for i in range(10,num_max_image + 1,10) :
-        print(f"Images genere par classes : {i}")
+        print(f"====== Images generated : {i*5} ======")
         #Incresing hidden layer number while decresing the number of neurons
-        for y in range(3) :
-            result = trainModel(n=num_model_training,n_ok=i,n_nok=i*4,layer_size=layer_sizing[y],classes=classes)
-            print(f"Average for {i} image, {str(layer_sizing[y])}: {result}")
-            #
-            # Add the collection of data here.
-            # Will need eval fonction ,average , Standard deviation, time of computation, number of images used, number of hidden layers, number of neurons(total) and how may classes  
-            # Collector.save()
-            #
+        for y in range(5) :
+            #Time the training of the model
+            start = time.time()
+            average, nRange = trainModel(n=num_model_training,n_ok=i,n_nok=i*4,layer_size=layer_sizing[y],classes=classes)
+            end = time.time()
+            print(f"Average and range for {i} image, {str(layer_sizing[y])}: {average} - {nRange}")
+            #Save data
+            Collector.save(evaluation=0,average=average,nRange=nRange,n_image=i*5,
+                n_layers=layer_size[y],n_neurons=sum(layer_sizing[y]),computation_time=(end-start))
 
 #Get the average of n time of the same model.
 def trainModel(n, n_ok=25, n_nok=100, layer_size=(220,), classes=2) :
+    #init data
+    total = 0
+    average = 0
+    nRange = 0
+    minimum = float('inf')
+    maximum = float('-inf')
+
+    #Generate images and convert them into arrays
     Generator.main(generateImage=True,ok=n_ok,nok=n_nok,color=None)
     array_2x, array_2y = Analyser.main(classes)
-    total = 0
+
+    #Using the same data set, train the same model n times to collect average and deviation
     for i in range(n) :
         result = Classifier.train(array_2x,array_2y, layer_size)
+        if result > maximum :
+            maximum = result
+        if result < minimum :
+            minimum = result
         total += result
+
+    #Delete the images folder for the next model
     deleteImageFolder()
-    return total / n
+
+    #Calculate the average and the deviation.
+    average = total / n
+    nRange = maximum - minimum
+    
+    return average, nRange
 
 def deleteImageFolder() :
     try:
@@ -54,6 +77,10 @@ def deleteImageFolder() :
 
 def main():
     iterate(num_max_image=200, classes=2, num_model_training=1)
+    
+    #Exemple of DataCollector.py
+    #Collector.save(10,10,10,10,10,10,10)
+    #Collector.deleteData()
 
 if __name__ ==  "__main__" :
     sys.exit(main())
